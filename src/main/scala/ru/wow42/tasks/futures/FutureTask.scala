6c5c8005be1a1b6e6740abd52e24e6d241d97e7d
wow42ru <wow42ru@mail.ru>
2020-03-24 17:47:24 +0300

    ++

--- src/main/scala/ru/wow42/tasks/futures/FutureTask.scala
+++ src/main/scala/ru/wow42/tasks/futures/FutureTask.scala
@@ -1,7 +1,10 @@
+package ru.wow42.tasks.futures
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
-case class Test[A]() {
+
+case class FutureTask[A]() {
   def traverse(list: List[Future[A]]): Future[(List[Throwable], List[A])] = {
     val seq: Future[List[Try[A]]] = Future.sequence(list.map(_.transform(Success(_))))
     val successes: Future[List[A]] = seq.map(_.collect { case Success(x) => x })
@@ -10,5 +13,3 @@ case class Test[A]() {
     result
   }
 }
-//first change
-val t: Future[(List[Throwable], List[Int])] =Test.apply().traverse(List[Future[Int]](Future(3),Future(2),Future(throw new Exception)))
