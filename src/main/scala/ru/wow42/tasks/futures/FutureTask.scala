1c6355c1d0fc1d03ce44d30054044e4d4a591cea
wow42ru <wow42ru@mail.ru>
2020-03-24 19:48:44 +0300

    ++

--- src/main/scala/ru/wow42/tasks/futures/FutureTask.scala
+++ src/main/scala/ru/wow42/tasks/futures/FutureTask.scala
@@ -9,7 +9,7 @@ case class FutureTask[A]() {
     val seq: Future[List[Try[A]]] = Future.sequence(list.map(_.transform(Success(_))))
     val successes: Future[List[A]] = seq.map(_.collect { case Success(x) => x })
     val failures: Future[List[Throwable]] = seq.map(_.collect { case Failure(x) => x })
-    val result: Future[(List[Throwable], List[A])] = failures.zip(successes)
-    result
+    val res: Future[(List[Throwable], List[A])] = failures.zip(successes)
+    res
   }
 }
